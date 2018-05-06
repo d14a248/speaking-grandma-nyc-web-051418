862ad2e4ecb19444e54baa3ec8b6a6fa4e05caca
david landau <thelandaus@gmail.com>
2018-05-06 20:58:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    puts "NO, NOT SINCE 1938!"
   elsif phrase == 'I LOVE YOU GRANDMA!'
     puts 'I LOVE YOU TOO PUMPKIN!'
   else 
-    
+    puts
   end
 end
